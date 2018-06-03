0df9aaec033b0bbb15adce86043896a1292fe381
Ruben Maurer <rubenmaurer@live.de>
2018-06-03 11:34:36 +0200

    #7 Add todo sorting
    Todos s are now sorted from high priority to low priority

--- src/main/java/de/swtproject/doit/core/Priority.java
+++ src/main/java/de/swtproject/doit/core/Priority.java
@@ -11,22 +11,22 @@ public enum Priority {
     /**
      * Default priority.
      */
-    DEFAULT("#FFFFFF", "Normal"),
+    DEFAULT("#FFFFFF", "Normal", 1),
 
     /**
      * Unimportant priority.
      */
-    UNIMPORTANT("#4DAF7C", "Unwichtig"),
+    UNIMPORTANT("#4DAF7C", "Unwichtig", 0),
 
     /**
      * Important priority.
      */
-    IMPORTANT("#F9690E", "Wichtig"),
+    IMPORTANT("#F9690E", "Wichtig", 2),
 
     /**
      * Urgent priority.
      */
-    URGENT("#9D2933", "Dringend");
+    URGENT("#9D2933", "Dringend", 3);
 
     /**
      * The Name.
@@ -39,13 +39,20 @@ public enum Priority {
     public final String color;
 
     /**
+     * The weight.
+     */
+    public final int weight;
+
+    /**
      * Instantiates a new Priority.
      *
      * @param color the color
      * @param name  the name
+     * @param weight  the weight
      */
-    Priority(String color, String name) {
+    Priority(String color, String name, int weight) {
         this.color = color;
         this.name = name;
+        this.weight = weight;
     }
 }
